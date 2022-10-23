27c0175c90a363dc37758dcc287eec8ff3fdd26b
John Kommala <johnwesleygithub@gmail.com>
2022-10-24 00:42:39 +0100

    Finished it up

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const axios = require('axios')
 const cors = require('cors');
 const express = require('express');
 const { get } = require("http");
-const path = require('path');
 
+// Setting up Express App
 const app = express();
 app.use(cors());
 const port = 3000
+
+// API Key from .env file and the base url
 const base_url = `https://api.openweathermap.org/data/2.5`
 const API_key = process.env.API_key
 
+// Some helper functions
 const average = arr => (arr.reduce((p, c) => p + c, 0) / arr.length).toFixed(2);
-// const average = arr => (arr.reduce((p, c) => p + c, 0) / arr.length);
 const sum = arr => (arr.reduce((p, c) => p + c, 0)).toFixed(2);
 const kelvin_to_celsius = k => (k < 0) ? '0K' : Math.round((k - 273.12) * 100) / 100;
 const min_max = (arr) => {
@@ -23,158 +25,134 @@ const min_max = (arr) => {
     return { min: min, max: max }
 }
 
-const town = 'Dublin'
 
 app.get('/', (req, res) => res.send('Weather App Server Side'));
 
-app.get('/forecast/:town', getForecast);
+app.get('/weather/:town', getForecast);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Weather app listening on port ${port}!`));
 
-function getMaskAdvise(forecastSummary) {
+// Air Pollution PM2_5 Analysis
+function getMaskAdvise(forecastData) {
     var pm2_5 = [];
-    for (dateEntry in forecastSummary) {
-        pm2_5.push(parseInt(forecastSummary[dateEntry].averagePM2_5));
+    for (forecastDate in forecastData) {
+        pm2_5.push(parseInt(forecastData[forecastDate].avgPM2_5));
     }
     const pm2_5_avg = average(pm2_5);
     return (pm2_5_avg > 10);
 }
 
-function getTemperaturesSummary(forecastSummary) {
+// Temperature Analysis - hot/warm/cold
+function getTemperatureAnalysis(forecastData) {
     let max = 0;
-    let min = forecastSummary[Object.keys(forecastSummary)[0]].averageTemp;
-    let sentiment = null;
+    let min = forecastData[Object.keys(forecastData)[0]].avgTemp;
+    let weatherType = null;
 
-    let minMaxObj = {};
+    let tempRange = {};
 
-    // Loop over every day getting the absolute min and max values
-    for (dateEntry in forecastSummary) {
-        minMaxObj = forecastSummary[dateEntry].temperatureRange;
-
-        // Check if the max on this day is more than current max
-        if (minMaxObj.max >= max)
-            max = minMaxObj.max;
-
-        // Check if the min on this day is more than current max
-        if (minMaxObj.min <= min)
-            min = minMaxObj.min;
-
+    for (forecastDate in forecastData) {
+        tempRange = forecastData[forecastDate].temperatureRange;
+        if (tempRange.max >= max)
+            max = tempRange.max;
+        if (tempRange.min <= min)
+            min = tempRange.min;
     }
 
-
-
-    if (max >= 20.0)
-        sentiment = "hot";
-
-    else if (max <= 20.0 && min >= 10.0)
-        sentiment = "warm";
-
-    else
-        sentiment = "cold";
-
+    if (max > 24) weatherType = "hot";
+    else if ( min >= 12 && max <= 24 ) weatherType = "mild";
+    else weatherType = "cold";
 
     return {
-        sentiment: sentiment,
+        weatherType: weatherType,
         max: max,
         min: min
     }
 
 }
 
-
-// function getAirPollutionForecast(lat, lon) {
-
-
-// }
-
 function getForecast(req, res) {
-    let town = req.params.town;
+    var town = req.params.town;
     console.log(`Generating weather forecast for town ${town}...`);
 
-    var forecastSummary = {};
-    let isRain = false;
-    let forecastSentiment = null;
-    let airPollutionSummary = {};
-    var lati = 0;
-    var longi = 0;
+    var forecastData = {};
+    var doesRain = false;
+    var airPollutionData = {};
+    var townLat = 0;
+    var townLon = 0;
 
 
     axios.get(`${base_url}/forecast?q=${town}&APPID=${API_key}`).then(
         (response) => {
             const { lat, lon } = response.data.city.coord;
-            lati = lat;
-            longi = lon;
-            // const airPollution = getAirPollutionForecast(lat, lon);
+            townLat = lat;
+            townLon = lon;
 
             var weatherData = response.data.list;
-
-            //  Loop over OpenWeather API response and extract data for each day
+            // Iterating over each day forecast
             for (weatherEntry in weatherData) {
-                // Make the date look nicer for front-end
+                // formatting date
                 let date = new Date(response.data.list[weatherEntry].dt * 1000);
                 date.setHours(0, 0, 0, 0);
                 date = date.toLocaleDateString();
 
-                // First check if there is a date entry for the given date, if not create one
-                if (!forecastSummary[date]) {
-                    forecastSummary[date] = {
-                        temperatures: [],
+                // Initiliazing if undefined or null
+                if (!forecastData[date]) {
+                    forecastData[date] = {
                         windSpeeds: [],
+                        temperatures: [],
                         rainfallLevels: [],
                     }
                 }
 
-                // Extract temperature and wind speed data
-                forecastSummary[date].temperatures.push(weatherData[weatherEntry].main.temp);
-                forecastSummary[date].windSpeeds.push(weatherData[weatherEntry].wind.speed);
+                forecastData[date].windSpeeds.push(weatherData[weatherEntry].wind.speed);
+                forecastData[date].temperatures.push(weatherData[weatherEntry].main.temp);
 
                 // Check if there is any rain
                 if (weatherData[weatherEntry].rain && weatherData[weatherEntry].rain['3h']) {
-                    isRain = true;
-                    forecastSummary[date].rainfallLevels.push(weatherData[weatherEntry].rain['3h']);
+                    doesRain = true;
+                    forecastData[date].rainfallLevels.push(weatherData[weatherEntry].rain['3h']);
                 }
 
             }
 
         }
     ).then(() => {
-        axios.get(`${base_url}/air_pollution/forecast?lat=${lati}&lon=${longi}&APPID=${API_key}`).then((response1) => {
+        axios.get(`${base_url}/air_pollution/forecast?lat=${townLat}&lon=${townLon}&APPID=${API_key}`).then((response1) => {
             const airPollutionData = response1.data.list;
             for (airPollutionEntry of airPollutionData) {
                 let date = new Date(airPollutionEntry.dt * 1000);
                 date.setHours(0, 0, 0, 0);
                 date = date.toLocaleDateString();
 
-                // First check if there is a date entry for the given date, if not create one
-                if (!airPollutionSummary[date]) {
-                    airPollutionSummary[date] = {
+                // Initiliazing if undefined or null
+                if (!airPollutionData[date]) {
+                    airPollutionData[date] = {
                         pm2_5: []
                     }
                 }
-                // Extract temperature and wind speed data
-                airPollutionSummary[date].pm2_5.push(airPollutionEntry.components.pm2_5);
+                airPollutionData[date].pm2_5.push(airPollutionEntry.components.pm2_5);
                 // console.log(`${date} - PM2_5 - ${airPollutionEntry.components.pm2_5}`)
             }
 
-            // When finished extracting data, calculate averages
-            for (dateEntry in forecastSummary) {
-                forecastSummary[dateEntry].averageTemp = kelvin_to_celsius(average(forecastSummary[dateEntry].temperatures));
-                forecastSummary[dateEntry].averageWind = average(forecastSummary[dateEntry].windSpeeds);
-                forecastSummary[dateEntry].rainfallLevels = sum(forecastSummary[dateEntry].rainfallLevels);
-                forecastSummary[dateEntry].temperatureRange = min_max(forecastSummary[dateEntry].temperatures);
-                forecastSummary[dateEntry].averagePM2_5 = average(airPollutionSummary[dateEntry].pm2_5);
+            //  Calculating averages once compiled
+            for (forecastDate in forecastData) {
+                forecastData[forecastDate].avgTemp = kelvin_to_celsius(average(forecastData[forecastDate].temperatures));
+                forecastData[forecastDate].temperatureRange = min_max(forecastData[forecastDate].temperatures);
+                forecastData[forecastDate].avgWind = average(forecastData[forecastDate].windSpeeds);
+                forecastData[forecastDate].rainfallLevels = sum(forecastData[forecastDate].rainfallLevels);
+                forecastData[forecastDate].avgPM2_5 = average(airPollutionData[forecastDate].pm2_5);
             }
 
-            // Get overall temperature sentiment
-            temperatureSummary = getTemperaturesSummary(forecastSummary);
-            maskAdvised = getMaskAdvise(forecastSummary);
+            // Get overall temperature weatherType and air pollution analysis
+            temperatureAnalysis = getTemperatureAnalysis(forecastData);
+            maskAdvised = getMaskAdvise(forecastData);
 
 
 
             res.json({
-                forecastSummary: forecastSummary,
-                isRain: isRain,
-                temperatureSummary: temperatureSummary,
+                forecastData: forecastData,
+                doesRain: doesRain,
+                temperatureAnalysis: temperatureAnalysis,
                 maskAdvised: maskAdvised
             })
 
@@ -195,10 +173,6 @@ function getForecast(req, res) {
             error: "Bad Request!"
         });
     })
-
-
-
-
 
 }
 
